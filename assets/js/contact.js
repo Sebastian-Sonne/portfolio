54e670f7c3c8d73baa95c58415c56bbd3d62ed6c
sebastian-sonne <sebastian@familie-sonne.com>
2024-01-18 09:29:56 -0600

    jquerie update

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -3,7 +3,16 @@ function submitForm() {
     element.style.width = "100%";
     element.style.transform = "skew(0deg)"
 
-    var content = document.getElementById("contact-form");
+    var content = document.getElementById("contact-main-content");
     content.style.transition = "display 0.3s ease";
     content.style.display = "none";
+
+    DelayNode
+
+    sleep(2000).then(() => {window.location.href = window.location.origin + '/'});
+}
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
