14368e0625c544211a80f109531b547d0a387bad
sebastian-sonne <sebastian@familie-sonne.com>
2024-01-19 19:02:40 -0600

    contact form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,13 +1,7 @@
 function submitForm() {
-    var element = document.getElementById("form-submit");
-    element.style.width = "100%";
-    element.style.transform = "skew(0deg)"
-
     var content = document.getElementById("contact-main-content");
     content.style.transition = "display 0.3s ease";
     content.style.display = "none";
-
-    DelayNode
 
     sleep(2000).then(() => {window.location.href = window.location.origin + '/'});
 }
