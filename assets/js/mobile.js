64cd68a50049c4bac0816f60cbc1a97c243d544e
sebastian-sonne <sebastian@familie-sonne.com>
2024-01-30 20:11:25 -0600

    bio update

--- assets/js/mobile.js
+++ assets/js/mobile.js
@@ -2,9 +2,11 @@ const doc = document;
 const menuOpen = doc.querySelector(".menu");
 const menuClose = doc.querySelector(".close");
 const overlay = doc.querySelector(".overlay");
+const bioContent = doc.getElementById("#bio-content");
 
 menuOpen.addEventListener("click", () => {
   overlay.classList.add("overlay--active");
+  bioContent.style.display = "none";
 });
 
 menuClose.addEventListener("click", () => {
