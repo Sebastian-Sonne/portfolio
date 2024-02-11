405cd4fc0b369673178a1fff702a12ab04da2e19
sebastian-sonne <sebastian@familie-sonne.com>
2024-02-11 15:48:42 -0600

    robots.txt add

--- assets/js/media.js
+++ assets/js/media.js
@@ -7,18 +7,23 @@ document.addEventListener("DOMContentLoaded", function () {
     categoryNav.addEventListener("click", function (event) {
         if (event.target.tagName === "LI") {
             const selectedCategory = event.target.dataset.category;
-            updateImages(selectedCategory);
-            updateURL(selectedCategory);
+            const prevCategory = getCategoryFromUrl();
 
-            const currentElement = document.querySelector(`[data-category="${selectedCategory}"]`);
+            //prevent unnesecary reload
+            if (selectedCategory != prevCategory) {
+                updateImages(selectedCategory);
+                updateURL(selectedCategory);
 
-            if (previousElement) {
-                previousElement.style.backgroundColor = '';
+                const currentElement = document.querySelector(`[data-category="${selectedCategory}"]`);
+
+                if (previousElement) {
+                    previousElement.style.backgroundColor = '';
+                }
+                currentElement.style.backgroundColor = 'var(--primary)';
+
+                previousElement = currentElement;
+                setupIntersectionObserver();
             }
-            currentElement.style.backgroundColor = 'var(--primary)';
-
-            previousElement = currentElement;
-            setupIntersectionObserver();
         }
     });
 
@@ -34,10 +39,12 @@ document.addEventListener("DOMContentLoaded", function () {
             const imgElement = document.createElement("img");
             imgElement.src = image;
             imgElement.classList.add('hidden');
+            imgElement.setAttribute('alt', 'Image in Image Galery - auto generated, no description available.');
             imageContainer.appendChild(imgElement);
         });
     }
 
+    //sets url-param "category" to the category parameter
     function updateURL(category) {
         const urlParams = new URLSearchParams(window.location.search);
         urlParams.set("category", category);
@@ -45,12 +52,13 @@ document.addEventListener("DOMContentLoaded", function () {
         window.history.pushState({ path: newUrl }, '', newUrl);
     }
 
+    //returns list of image urls depending on category
     function getImagesForCategory(category) {
         // Simulated image URLs for demonstration
         const imageUrls = {
             all: ["/img/b-roll/mountain_2.jpg", "/img/b-roll/singapore_1.webp", "/img/b-roll/snow_1.jpg", "/img/b-roll/sunset_1.jpg", "/img/b-roll/tennis_1.webp", "/img/b-roll/munich_1.webp", "/img/b-roll/mountain_2.jpg", "/img/b-roll/singapore_1.webp", "/img/b-roll/snow_1.jpg", "/img/b-roll/sunset_1.jpg", "/img/b-roll/tennis_1.webp", "/img/b-roll/munich_1.webp", "/img/b-roll/mountain_2.jpg", "/img/b-roll/singapore_1.webp", "/img/b-roll/snow_1.jpg", "/img/b-roll/sunset_1.jpg", "/img/b-roll/tennis_1.webp", "/img/b-roll/munich_1.webp", "/img/b-roll/mountain_2.jpg", "/img/b-roll/singapore_1.webp", "/img/b-roll/snow_1.jpg", "/img/b-roll/sunset_1.jpg", "/img/b-roll/tennis_1.webp", "/img/b-roll/munich_1.webp"],
-            nature: ["landscape1.jpg", "landscape2.jpg"],
-            city: ["portrait1.jpg", "portrait2.jpg"],
+            nature: ["/img/b-roll/mountain_2.jpg", "/img/b-roll/snow_1.jpg", "/img/b-roll/sunset_1.jpg"],
+            city: ["/img/b-roll/munich_1.webp", "/img/b-roll/singapore_1.webp"],
             birdseye: ["", ""]
             // Add more categories as needed
         };
@@ -68,7 +76,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const initialCategory = getCategoryFromUrl();
     const initialNavItem = categoryNav.querySelector(`[data-category="${initialCategory}"]`);
     if (initialNavItem) {
-        initialNavItem.classList.add("selected"); // Add a class to highlight the initially selected category
+        initialNavItem.style.backgroundColor = 'var(--primary';
+        previousElement = initialNavItem;
     }
     updateImages(initialCategory);
 });
@@ -88,4 +97,4 @@ function setupIntersectionObserver() {
     hiddenElements.forEach((el) => observer.observe(el));
 }
 
-window.addEventListener('load', setupIntersectionObserver); // Call the setup function on initial page load
+window.addEventListener('load', setupIntersectionObserver);
