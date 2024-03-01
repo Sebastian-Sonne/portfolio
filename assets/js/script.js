4fa23498132ff8097295a833b8c4a4251dd1b293
sebastian-sonne <sebastian@familie-sonne.com>
2024-02-29 20:12:38 -0600

    url param theme added

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,10 +16,47 @@ function toggleTheme() {
     themeIcon.textContent = newTheme === 'light' ? 'light_mode' : 'dark_mode';
 }
 
+function checkUrlForTheme() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const theme = urlParams.get('theme');
+    const url = new URL(window.location.href);
+
+    if (theme == 'light') {
+        removeThemeFromUrl(urlParams);    
+        return 'light';
+    } else if (theme == 'dark') {
+        removeThemeFromUrl(urlParams);    
+        return 'dark';
+    }
+
+    removeThemeFromUrl(urlParams);
+    return null;
+}
+
+function removeThemeFromUrl(urlParams) {
+    urlParams.delete('theme');
+
+    let newUrl = window.location.pathname;
+    if (urlParams.toString() !== '') {
+        newUrl += '?' + urlParams.toString();
+    }
+
+    history.replaceState(null, '', newUrl);
+}
+
 // Set initial theme and icon
 document.addEventListener('DOMContentLoaded', (event) => {
-    const savedTheme = localStorage.getItem('theme') || 'light';
-    document.documentElement.setAttribute('data-theme', savedTheme);
+    const urlTheme = checkUrlForTheme();
+    let savedTheme;
+
+    if (urlTheme != null) {
+        savedTheme = urlTheme;
+        document.documentElement.setAttribute('data-theme', urlTheme);
+        localStorage.setItem('theme', urlTheme);
+    } else {
+        savedTheme = localStorage.getItem('theme') || 'light';
+        document.documentElement.setAttribute('data-theme', savedTheme);
+    }
 
     const themeIcon = document.getElementById('theme-icon');
     themeIcon.textContent = savedTheme === 'light' ? 'light_mode' : 'dark_mode';
